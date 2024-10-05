3aed6a1309e0937d570295c2371dd1089914f8bb
alhinator <alex@leghart.org>
2024-10-05 15:14:45 -0700

    step 1
    "incremental design" step 1 added interactive button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const clickerBar = document.createElement("h2");
+const mainClicker = document.createElement("button");
+mainClicker.innerHTML = "click me 💢";
+clickerBar.append(mainClicker);
+app.append(clickerBar);
