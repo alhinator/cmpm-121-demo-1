ffc940d2bd6027d73a40aebcfe830223e11187bf
alhinator <alex@leghart.org>
2024-10-07 16:23:51 -0700

    got button and buy working step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ function incScore(add:number):void{
     mainScore += add
     scoreDisplay = mainScore.toLocaleString('fullwide', {maximumFractionDigits:2})
     counterField.innerText = "Orteils: " + scoreDisplay
+    //now verify buttons
+    ShopButton.verifyAllButtons()
 }
 
 //add the clicker bar, clicker, and set the default value for clicks.
@@ -33,6 +35,9 @@ const counterField = document.createElement("div");
 counterField.innerText = "Orteils: " + mainScore;
 app.append(counterField)
 
+//add the field that holds buttons
+const buttonField = document.createElement("div");
+app.append(buttonField)
 
 //automatic scoring
 //template for autoscore source
@@ -105,9 +110,8 @@ class AutoManager{
 }
 //NO LONGER ADD DEFAULT
 //need to add smth to verify no duplicates. will add during purchase stage
-AutoManager.addSource(new AutoScoreSource("a", 1, 1))
-AutoManager.addSource(new AutoScoreSource("a", 1, 1))
-console.log(AutoManager.sources)
+// AutoManager.addSource(new AutoScoreSource("a", 1, 1))
+// AutoManager.addSource(new AutoScoreSource("a", 1, 1))
 
 let frameCounterTimer = 0;
 requestAnimationFrame(AutoManager.incAutoScores)
@@ -119,34 +123,56 @@ requestAnimationFrame(AutoManager.incAutoScores)
 
 //SHOP
 
-// class ShopButton{
-//     protected privName:string;
-//     protected privValue:number;
-//     protected privCost:number;
-//     protected privButtonElement:HTMLButtonElement;
-//     constructor(_name:string, _value:number, _cost:number, _parent_DOM:HTMLElement){
-//         this.privName = _name;
-//         this.privValue = _value;
-//         this.privCost = _cost;
-//         this.privButtonElement = document.createElement("button")
-//         _parent_DOM.appendChild(this.privButtonElement)
-//         this.privButtonElement.innerText = "Purchase" + this.name + " for " + this.cost + " Orteils."
-//         this.privButtonElement.onclick = function (){
-//             //purchase
-//             //this.
-//         }
-//     }
-//     //accessors
-//     get name():string{
-//         return this.privName
-//     }
-//     get value():number{
-//         return this.privValue;
-//     }
-//     get cost():number{
-//         return this.privCost;
-//     }
-// }
+class ShopButton{
+    static allShopButtons:ShopButton[] = [];
+    protected privName:string;
+    protected privValue:number;
+    protected privCost:number;
+    protected privButtonElement:HTMLButtonElement;
+    constructor(_name:string, _value:number, _cost:number, _parent_DOM:HTMLElement){
+        this.privName = _name;
+        this.privValue = _value;
+        this.privCost = _cost;
+        this.privButtonElement = document.createElement("button")
+        _parent_DOM.appendChild(this.privButtonElement)
+        this.privButtonElement.innerText = "Purchase " + this.name + " (" + _value + " O/s) for " + this.cost + " Orteils."
+        this.privButtonElement.onclick = function (){ShopButton.purchaseAButton(_name, _value, _cost)}
+        ShopButton.allShopButtons.push(this)
+    }
+    //accessors
+    get name():string{
+        return this.privName
+    }
+    get value():number{
+        return this.privValue;
+    }
+    get cost():number{
+        return this.privCost;
+    }
+    //helpers
+    static purchaseAButton(_name:string, _value:number, _cost:number){
+        console.log("in purchaseAbutton")
+            AutoManager.addSource(new AutoScoreSource(_name, _value, 1))
+            incScore(-_cost)
+            console.log(AutoManager.sources)
+            
+    }
+    toggleClickable(){
+        if (mainScore >= this.cost){
+            this.privButtonElement.disabled = false;
+        } else {
+            this.privButtonElement.disabled = true;
+        }
+    }
+
+    static verifyAllButtons(){
+        ShopButton.allShopButtons.forEach(element => {
+            element.toggleClickable()
+        });
+    }
+}
 
 
+new ShopButton("France", 1, 10, buttonField)
 
+ShopButton.verifyAllButtons()
