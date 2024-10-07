6175c257b2339888edcd516f4dffcbbf71f623c3
alhinator <alex@leghart.org>
2024-10-07 12:02:07 -0700

    step 2: counter

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,28 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "fortnite clicker";
+const gameName = "orteil42 clicker";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let mainScore:number = 0;
+
 const clickerBar = document.createElement("h2");
 const mainClicker = document.createElement("button");
-mainClicker.innerHTML = "click me 💢";
+const clickValue = 1;
+mainClicker.onclick = ()=> {incCounter(clickValue)}
+mainClicker.innerHTML = "get more orteil42 🥖";
 clickerBar.append(mainClicker);
 app.append(clickerBar);
+
+const counterField = document.createElement("div");
+counterField.innerText = "Orteils: " + mainScore;
+app.append(counterField)
+
+function incCounter(add:number):void{ 
+    mainScore += add
+    counterField.innerText = "Orteils: " + mainScore
+}
