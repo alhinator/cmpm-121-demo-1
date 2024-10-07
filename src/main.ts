4c96ce42a8872707d854775999ec2ccbe2370f58
alhinator <alex@leghart.org>
2024-10-07 12:20:46 -0700

    step 3: auto score

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,65 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//this score is the main score, should never be modified directly, rather use incScore
 let mainScore:number = 0;
+//mainScore access func
+function incScore(add:number):void{ 
+    mainScore += add
+    counterField.innerText = "Orteils: " + mainScore
+}
 
+//add the clicker bar, clicker, and set the default value for clicks.
+const clickValue = 1;
 const clickerBar = document.createElement("h2");
 const mainClicker = document.createElement("button");
-const clickValue = 1;
-mainClicker.onclick = ()=> {incCounter(clickValue)}
+mainClicker.onclick = ()=> {incScore(clickValue)}
 mainClicker.innerHTML = "get more orteil42 🥖";
 clickerBar.append(mainClicker);
 app.append(clickerBar);
 
+//add the field that tracks score
 const counterField = document.createElement("div");
 counterField.innerText = "Orteils: " + mainScore;
 app.append(counterField)
 
-function incCounter(add:number):void{ 
-    mainScore += add
-    counterField.innerText = "Orteils: " + mainScore
+
+//automatic scoring
+//template for autoscore source
+class AutoScoreSource{
+    name:string;
+    baseValue:number;
+    quantity:number;
+    constructor(_name:string, _value:number, _quantity:number){
+        this.name = _name;
+        this.baseValue = _value;
+        this.quantity = _quantity;
+    }
+    //accessors
+    public getName():string{
+        return this.name;
+    }
+    public getValue():number{
+        return this.baseValue * this.quantity;
+    }
+    public getQuantity():number{
+        return this.quantity
+    }
+    //setters
+    public createMore(_q:number){
+        this.quantity += _q
+    }
+}
+const autoScoreSources:AutoScoreSource[] = []
+//add default:1
+//need to add smth to verify no duplicates. will add during purchase stage
+autoScoreSources.push(new AutoScoreSource("default", 1, 1))
+setInterval(incAutoScores, 1000)
+
+function incAutoScores(_arr:AutoScoreSource[] = autoScoreSources){
+    let sum = 0;
+    _arr.forEach(element => {
+        sum += element.getValue()
+    });
+    incScore(sum)
 }
